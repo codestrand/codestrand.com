0ccfc623dd3487a747f21b31aa6774f7985bbdea
Ana Cidre <ana@anacidre.com>
2018-03-16 13:06:54 +0100

    Add about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,6 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'cs-about',
   template: `<div id="about" class="about">
     <h2>About us</h2>
+    <div class="about__container">
+      <div class="about__item">
+        <p>We are blah blah and we do blah blah</p>
+      </div>
+      <div class="about__image">
+        <img src="./../../assets/images/angular/angular_whiteTransparent.svg">
+      </div>
+    </div>
   </div>`,
   styleUrls: ['./about.component.sass']
 })
