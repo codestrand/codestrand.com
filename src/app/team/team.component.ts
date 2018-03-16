8597b93845d9abe831033f6fc4e25972d9ccbab4
Ana Cidre <ana@anacidre.com>
2018-03-16 13:08:17 +0100

    Add team component

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -4,6 +4,18 @@ import { Component, OnInit } from '@angular/core';
   selector: 'cs-team',
   template: `<div id="team" class="team">
     <h2>Team</h2>
+    <div class="team__container">
+      <div class="team__image_1">
+        <img src="./../../assets/images/team/orestes.jpg">
+        <h3>Orestes Carracedo</h3>
+        <p>Text about Orestes</p>
+      </div>
+      <div class="team__image_2">
+        <img src="./../../assets/images/team/ana.jpg">
+        <h3>Ana Cidre</h3>
+        <p>Text about Ana</p>
+      </div>
+    </div>
   </div>`,
   styleUrls: ['./team.component.sass']
 })
