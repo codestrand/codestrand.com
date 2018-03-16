5168b110d1dc6777c2a09c3589557d61ea1e6015
Ana Cidre <ana@anacidre.com>
2018-03-16 13:07:50 +0100

    Add contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,6 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'cs-contact',
   template: `<div id="contact" class="contact">
     <h2>Contact</h2>
+    <div class="contact__container">
+      <div class="contact__image">
+        <img src="./../../assets/images/brand/codestrand-bw.svg">
+      </div>
+      <div class="contact__item">
+        <p>We are blah blah and we do blah blah</p>
+      </div>
+    </div>
   </div>`,
   styleUrls: ['./contact.component.sass']
 })
