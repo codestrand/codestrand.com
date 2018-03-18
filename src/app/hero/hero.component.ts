42069d3929049fe52648f4e2768b29b3f0063e73
Orestes Carracedo <orestes@orestes.io>
2018-03-19 00:50:44 +0100

    Simplify Hero content

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,19 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cs-hero',
   template: `
     <div class="container">
       <img src="/assets/images/brand/codestrand.svg" class="homepage-logo">
-      <h2>Software development</h2>
+      <h2>Software Development</h2>
     </div>`,
   styleUrls: ['./hero.component.sass']
 })
-export class HeroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HeroComponent { }
